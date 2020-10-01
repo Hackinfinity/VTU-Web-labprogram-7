23a89f2f65fc00d8b7798c32543f86fd2788c896
Nikhil <aminnikhil073@gmail.com>
2020-10-02 00:59:30 +0530

    UI changes and real time update

--- Program7-ServTime.php
+++ Program7-ServTime.php
@@ -1,7 +1,31 @@
+<!-- 
+	Write a PHP program to display a digital clock which displays
+	the current time of the server.
+-->
+
 <?php
 
-date_default_timezone_set('Asia/Kolkata'); 
+    date_default_timezone_set('Asia/Kolkata');
 
-echo date('d/m/Y == h:i:s A');
+    function getTime() {
+        return date("g:i:s A", time());
+    }
 
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Digital Clock</title>
+        <meta http-equiv="Refresh" content="1">
+    </head>
+    <body style="background-color: #eeeeee;">
+        <div style="text-align: center; margin-top: 150px;">
+            <h2>Time:
+                <span style="color: red;">
+                    <?php echo getTime() ?>
+                </span>
+            </h2>
+        </div>
+    </body>
+</html>
